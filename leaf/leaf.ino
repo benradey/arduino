0b6dd4337f9cbed20cecb054cba54dd98f155070
Ben Radey <ben@radey.org>
2017-04-02 12:07:41 -0400

    Working accel, lights, and relay

--- leaf/leaf.ino
+++ leaf/leaf.ino
@@ -5,17 +5,47 @@
 #include <Adafruit_Sensor.h>
 
 #define LIS3DH_CS 10
+#define LED_GND 8
+#define LED_R 5
+#define LED_Y 3
+#define LED_G 6
+#define RELAY_GND 0
+#define RELAY_PWR 7
+#define RELAY_IN1 2
+#define RELAY_IN2 4
+
 #define LIGHT_ON 0.2
 #define LIGHT_OFF 0.1
+#define LED_DUTY_CYCLE 10
+#define LED_DC_YELLOW_MOD 20
 
 Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
 
 boolean lit = false;
 
+#define AVG_LENGTH 20
+double avg[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 void setup() {
 
   Serial.begin(9600);
   Serial.println("LIS3DH BEN test!");
+
+  pinMode(LED_GND, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_Y, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(RELAY_GND, OUTPUT);
+  pinMode(RELAY_PWR, OUTPUT);
+  pinMode(RELAY_IN1, OUTPUT);
+  pinMode(RELAY_IN2, OUTPUT);
+
+  digitalWrite(LED_GND, LOW);
+  digitalWrite(RELAY_GND, LOW);
+  digitalWrite(RELAY_IN1, HIGH);
+  digitalWrite(RELAY_IN2, HIGH);
+  digitalWrite(RELAY_PWR, HIGH);
+  analogWrite(LED_G, LED_DUTY_CYCLE);
   
   if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
     Serial.println("Couldnt start");
@@ -54,12 +84,33 @@ void loop() {
 
   double xg = lis.x / (double) 0x3fff;
 
-  if (lit && xg < LIGHT_OFF) {
+  if (xg > LIGHT_ON) {
+    analogWrite(LED_Y, LED_DUTY_CYCLE + LED_DC_YELLOW_MOD);
+  } else if (xg < LIGHT_OFF) {
+    analogWrite(LED_Y, 0);
+  }
+
+  double avgg = 0;
+  for (int i = AVG_LENGTH - 1 ; i > 0 ; i--) {
+    avg[i] = avg[i-1];
+    avgg += avg[i];
+  }
+
+  avg[0] = xg;
+  avgg += xg;
+  avgg = avgg / AVG_LENGTH;
+  
+
+  if (lit && avgg < LIGHT_OFF) {
     Serial.println("Lights off");
+    analogWrite(LED_R, 0);
+    digitalWrite(RELAY_IN1, HIGH);
     lit = false;
   }
-  if (!lit && xg > LIGHT_ON) {
+  if (!lit && avgg > LIGHT_ON) {
     Serial.println("Lights on");
+    analogWrite(LED_R, LED_DUTY_CYCLE);
+    digitalWrite(RELAY_IN1, LOW);
     lit = true;
   }
   
